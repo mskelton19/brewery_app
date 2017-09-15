1fc63708648a989207fe1d6a3e6b5ea883316f77
Mike Skelton <skeltonmr43@gmail.com>
2017-09-15 09:04:03 -0400

    setting up files

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Server
+const express   =   require ('express');
+const app       =   express();
+const mongoose  =   require('mongoose');
+
+
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/breweries';
+mongoose.connect(mongoUri);
+
+mongoose.connection.once('open', () => {
+	console.log('connected to mongo');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+	console.log('listening');
+});
