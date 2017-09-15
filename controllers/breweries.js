4f3ddac79db378435d654880c1408d135365e043
Mike Skelton <skeltonmr43@gmail.com>
2017-09-15 17:23:55 -0400

    connected with breweryDB API

--- controllers/breweries.js
+++ controllers/breweries.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const Pets = require('../models/breweries.js');
+const Brewery = require('../models/breweries.js');
+const rp = require('request-promise');
+// const BreweryDb = require('node-brewerydb');
+// const client = new BreweryDb ({apiKey: '18332a604bfda0ef56cfa29deff412f2'});
 
-const findBreweries = require('../bin/breweries.js');
+// const findBrewery = require('../bin/breweries.js');
 
-router.post('/findBreweries', (req, res) => {
-	console.log(res);
+router.get('/', (req, res) => {
+  rp({uri: 'http://api.brewerydb.com/v2/brewery/random?key=18332a604bfda0ef56cfa29deff412f2', json: true})
+    .then(function (response) {
+      res.send(response)
+        // Process html...
+    })
+    .catch(function (err) {
+      console.log(err)
+        // Crawling failed...
+    });
+  // res.send('hello');
+  // client.beers({name: 'Tecate'}).then(function(res) {
+  //   res.send(res);
+  // }, function(err) {
+  //   res.send(err)
+  // });
 })
 
 module.exports = router;
