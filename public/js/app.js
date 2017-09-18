d7f744353018a8819e1b05e12c7035b45b3714dc
Mike Skelton <skeltonmr43@gmail.com>
2017-09-17 21:05:01 -0400

    removed bin. trying to display API data on index.html

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,18 @@ const app   =   angular.module('BrewFinder', []);
 
 app.controller('BreweryController', ['$http', function($http) {
   const controller = this;
+  this.breweries = [];
 
   this.findBreweries = function(){
     $http({
       method: 'GET',
       url: '/breweries',
+      data: {
+        name: this.name
+      }
     }).then(function(response){
+      console.log(response)
+      this.foundBrewery = response.data
       console.log(response.data.data.name)
     },
       function(err){
