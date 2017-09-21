971bbfd6128be28e27339dae55dde1c819834e47
Mike Skelton <skeltonmr43@gmail.com>
2017-09-20 23:10:38 -0400

    created user landing page with info

--- controllers/users.js
+++ controllers/users.js
@@ -8,45 +8,39 @@ const methodOverride  = require('method-override');
 router.use(bodyParser.urlencoded({extended: false}));
 router.use(methodOverride('_method'));
 
-router.get('/', function(req, res){
-  User.find({}, (error, foundUser) => {
-    res.render('users/index.ejs', {
-      user: foundUser
-    });
-  });
-});
-
-router.put('/:id', (req, res) => {
-  User.findByIdAndUpdate(req.params.id, req.body, (error, updateModel) => {
-    res.redirect('/users')
+router.get('/', (req, res) => {
+  User.find({}, (err, foundUsers) => {
+    res.render('users/index.ejs',{
+      users: foundUsers
+    })
   })
 })
-
-router.post('/', (req, res) => {
-  User.create(req.body, (error, createdUser) => {
-    res.rediect('/users')
-  });
-});
 
 router.get('/new', (req, res) => {
   res.render('users/new.ejs');
 });
 
-router.get('/:id', (req, res) => {
-  User.findById(req.params.id, (error, User) => {
-    res.render('users/show.ejs', {
-      user: editUser
-    })
+router.post('/', (req, res) => {
+  User.create(req.body, (err, createdUser) => {
+    res.send(createdUser)
   })
-})
+});
 
-router.get('/:id/edit', (req, res) => {
-  User.findById(req.params.id, (error, editMember) => {
-    res.render('users/edit.ejs', {
-      user: editUser
-    })
-  })
-})
+// router.get('/:id', (req, res) => {
+//   User.findById(req.params.id, (error, User) => {
+//     res.render('users/show.ejs', {
+//       user: editUser
+//     })
+//   })
+// })
+//
+// router.get('/:id/edit', (req, res) => {
+//   User.findById(req.params.id, (error, editMember) => {
+//     res.render('users/edit.ejs', {
+//       user: editUser
+//     })
+//   })
+// })
 
 
 
